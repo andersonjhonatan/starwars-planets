8291496b7d5c92db0b367962fe84f05a3d070487
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-03-05 01:47:55 -0300

    Enviando os testes da 8 e 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
-import MockData from './mockData';
+import MockData from './mockData'
 import { act } from 'react-dom/test-utils';
 
 
-describe ('Verifica se é possível aplicar os filtros numéricos', () => {
+describe ('Filtros numéricos', () => {
 
   beforeEach(async() => {
     jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
@@ -26,155 +26,167 @@ describe ('Verifica se é possível aplicar os filtros numéricos', () => {
     jest.restoreAllMocks();
   });
 
-  it('Verifica se é numérico', async () => {
+  it('Filtro numérico', async () => {
 
-    const filterByNume = screen.getByTestId('column-filter');
-    const filCompa = screen.getByTestId('comparison-filter');
-    const filByVa = screen.getByTestId('value-filter');
-    const button = screen.getByTestId('button-filter');
+
+    const filterByNumeric = screen.getByTestId('column-filter');
+    expect(filterByNumeric).toBeInTheDocument();
+
+    const filterByCompa = screen.getByTestId('comparison-filter');
+    expect(filterByCompa).toBeInTheDocument();
+
+    const filterByV = screen.getByTestId('value-filter');
+    expect(filterByV).toBeInTheDocument();
+
+    const Button = screen.getByTestId('button-filter');
+    expect(Button).toBeInTheDocument();
+
     const ButtRem = screen.getByTestId('button-remove-filters');
-
-    expect(filterByNume).toBeInTheDocument();
-
-    expect(filCompa).toBeInTheDocument();
-
-    expect(filByVa).toBeInTheDocument();
-
-    expect(button).toBeInTheDocument();
-
     expect(ButtRem).toBeInTheDocument();
 
   });
 
-  it('Verifica se é possível aplicar o filtro por nome', async () => {
+  it('Filtro por nome', async () => {
 
-    const filterName = screen.getByTestId('name-filter');
-    userEvent.type(filterName, 'Tatooine');
+    const filterByName = screen.getByTestId('name-filter');
+    userEvent.type(filterByName, 'Tatooine');
 
   await waitFor(() => {
-    const PlaneTName = screen.getAllByTestId('planet-name')
-    expect(PlaneTName).toHaveLength(1);
+    const PlanetNam = screen.getAllByTestId('planet-name')
+    expect(PlanetNam).toHaveLength(1);
   });
 
 });
 
-  it('Verifica se é possível aplicar o filtro por coluna', async () => {
+  it('Filtro por coluna', async () => {
 
-    const filCompa = screen.getByTestId('comparison-filter');
-    const filtColumn = screen.getByTestId('column-filter');
-    const filByVa = screen.getByTestId('value-filter')
-    const button = screen.getByTestId('button-filter');
-    const ButtRem = screen.getByRole('button', { name: /X/i });
-    
-    expect(filtColumn).toBeInTheDocument();
-    expect(filtColumn).toHaveValue('population');
-    expect(filtColumn).toHaveLength(5);
 
-    userEvent.selectOptions(filtColumn, 'orbital_period');
-    expect(filtColumn).toHaveValue('orbital_period');
+    const filterCol = screen.getByTestId('column-filter');
+    expect(filterCol).toBeInTheDocument();
+    expect(filterCol).toHaveValue('population');
+    expect(filterCol).toHaveLength(5);
 
-    expect(filCompa).toBeInTheDocument();
-    expect(filCompa).toHaveValue('maior que');
-    expect(filCompa).toHaveLength(3);
+    userEvent.selectOptions(filterCol, 'orbital_period');
+    expect(filterCol).toHaveValue('orbital_period');
 
-    userEvent.selectOptions(filCompa, 'menor que');
-    expect(filCompa).toHaveValue('menor que');
+    const filterByCompa = screen.getByTestId('comparison-filter');
+    expect(filterByCompa).toBeInTheDocument();
+    expect(filterByCompa).toHaveValue('maior que');
+    expect(filterByCompa).toHaveLength(3);
 
-    expect(filByVa).toBeInTheDocument();
-    expect(filByVa).toHaveValue(0);
+    userEvent.selectOptions(filterByCompa, 'menor que');
+    expect(filterByCompa).toHaveValue('menor que');
 
-    userEvent.type(filByVa, '305');
+    const filterByV = screen.getByTestId('value-filter')
+    expect(filterByV).toBeInTheDocument();
+    expect(filterByV).toHaveValue(0);
 
-    expect(button).toBeInTheDocument();
-    userEvent.click(button);
+    userEvent.type(filterByV, '305');
+
+    const Button = screen.getByTestId('button-filter');
+    expect(Button).toBeInTheDocument();
+    userEvent.click(Button);
 
     await waitFor(() => {
-      const PlanetsName = screen.getAllByTestId('planet-name');
-      expect(PlanetsName).toHaveLength(1);
+      const PlanetNam = screen.getAllByTestId('planet-name');
+      expect(PlanetNam).toHaveLength(1);
     });
 
+    const ButtRem = screen.getByRole('button', { name: /X/i });
     expect(ButtRem).toBeInTheDocument();
-    expect(filtColumn).toHaveLength(4);
-    expect(filtColumn).toHaveValue('population');
+    expect(filterCol).toHaveLength(4);
+    expect(filterCol).toHaveValue('population');
 
     userEvent.click(ButtRem);
 
-    expect(filtColumn).toHaveLength(5);
-    expect(filtColumn).toHaveValue('population');
+    expect(filterCol).toHaveLength(5);
+    expect(filterCol).toHaveValue('population');
   });
 
-  it('Verifica o botão de remover filtros', async () => {
+  it('Botão de remover filtros', async () => {
 
-    const button = screen.getByTestId('button-filter');
+
+
+    const Button = screen.getByTestId('button-filter');
     const ButtRem = screen.getByTestId('button-remove-filters');
-    const filtColumn = screen.getByTestId('column-filter');
-    const filCompa = screen.getByTestId('comparison-filter');
-    const filByVa = screen.getByTestId('value-filter');
-    expect(filtColumn).toBeInTheDocument();
+    const filterCol = screen.getByTestId('column-filter');
+    expect(filterCol).toBeInTheDocument();
 
-    userEvent.selectOptions(filtColumn, 'orbital_period');
-    expect(filtColumn).toHaveValue('orbital_period');
+    userEvent.selectOptions(filterCol, 'orbital_period');
+    expect(filterCol).toHaveValue('orbital_period');
 
-    userEvent.selectOptions(filCompa, 'menor que');
-    expect(filCompa).toHaveValue('menor que');
+    const filterByCompa = screen.getByTestId('comparison-filter');
 
-    expect(filByVa).toBeInTheDocument();
-    expect(filByVa).toHaveValue(0);
+    userEvent.selectOptions(filterByCompa, 'menor que');
+    expect(filterByCompa).toHaveValue('menor que');
+
+    const filterByV = screen.getByTestId('value-filter');
+    expect(filterByV).toBeInTheDocument();
+    expect(filterByV).toHaveValue(0);
 
 
-    userEvent.type(filByVa, '4000');
+    userEvent.type(filterByV, '4000');
 
-    userEvent.click(button);
+    userEvent.click(Button);
 
-    userEvent.selectOptions(filtColumn, 'surface_water');
-    userEvent.selectOptions(filCompa, 'menor que');
-    userEvent.type(filByVa, '100');
-    userEvent.click(button);
 
-    userEvent.selectOptions(filtColumn, 'population');
-    userEvent.selectOptions(filCompa, 'maior que');
-    userEvent.type(filByVa, '30000000');
-    userEvent.click(button);
+    userEvent.selectOptions(filterCol, 'surface_water');
+    userEvent.selectOptions(filterByCompa, 'menor que');
+    userEvent.type(filterByV, '100');
+    userEvent.click(Button);
 
-    userEvent.selectOptions(filtColumn, 'diameter');
-    userEvent.selectOptions(filCompa, 'igual a');
-    userEvent.type(filByVa, '0');
-    userEvent.click(button);
+    userEvent.selectOptions(filterCol, 'population');
+    userEvent.selectOptions(filterByCompa, 'maior que');
+    userEvent.type(filterByV, '30000000');
+    userEvent.click(Button);
+
+
+    userEvent.selectOptions(filterCol, 'diameter');
+    userEvent.selectOptions(filterByCompa, 'igual a');
+    userEvent.type(filterByV, '0');
+    userEvent.click(Button);
 
     await waitFor(() => {
-      const PlanetsName =  screen.queryAllByTestId('planet-name')
-      expect(PlanetsName).toHaveLength(0);
+      const PlanetNam =  screen.queryAllByTestId('planet-name')
+      expect(PlanetNam).toHaveLength(0);
     });
 
-    expect(filtColumn).toHaveLength(1);
+    expect(filterCol).toHaveLength(1);
 
     userEvent.click(ButtRem);
 
-    expect(filtColumn).toHaveLength(5);
+    expect(filterCol).toHaveLength(5);
 
   });
 
-  it('Verifica se é possível aplicar o filtro por coluna', async () => {
+  it('Verifica se é possível aplicar o filtro por coluna e nome', async () => {
 
-    const PlanetsName = screen.getAllByTestId('planet-name');
-    const filterName = screen.getByTestId('column-filter');
-    const filByVa = screen.getByTestId('value-filter');
-    const filCompa = screen.getByTestId('comparison-filter');
-    const button = screen.getByTestId('button-filter');
-    const filter = screen.getByTestId('filter');
+
+      const PlanetNam = screen.getAllByTestId('planet-name');
+      expect(PlanetNam).toHaveLength(10);
+
+    const filterByName = screen.getByTestId('column-filter');
+    userEvent.selectOptions(filterByName, 'orbital_period');
+    expect(filterByName).toBeInTheDocument();
+
+    const filterByV = screen.getByTestId('value-filter');
+    userEvent.type(filterByV, '3');
+    expect(filterByV).toBeInTheDocument();
+
+    const filterByCompa = screen.getByTestId('comparison-filter');
+    userEvent.selectOptions(filterByCompa, 'maior que');
+    expect(filterByCompa).toBeInTheDocument();
+
+    const Button = screen.getByTestId('button-filter');
+    userEvent.click(Button);
+    expect(Button).toBeInTheDocument();
+
+
+  const filter = screen.getByTestId('filter');
+  expect(filter).toHaveTextContent(/orbital_period/i);
+
+
     const ButtRem = screen.getByRole('button', { name: /X/i });
-
-    expect(PlanetsName).toHaveLength(10);
-    userEvent.selectOptions(filterName, 'orbital_period');
-    expect(filterName).toBeInTheDocument();
-    userEvent.type(filByVa, '3');
-    expect(filByVa).toBeInTheDocument();
-    userEvent.selectOptions(filCompa, 'maior que');
-    expect(filCompa).toBeInTheDocument();
-    userEvent.click(button);
-    expect(button).toBeInTheDocument();
-
-    expect(filter).toHaveTextContent(/orbital_period/i);
     userEvent.click(ButtRem);
 
   });
@@ -182,19 +194,16 @@ describe ('Verifica se é possível aplicar os filtros numéricos', () => {
   it('Verifica se ordena em ascendente e descendente', async () => {
 
     const btnOrder = screen.getByTestId('column-sort-button')
-    const btnOrderAsc = screen.getByTestId('column-sort-input-asc')
-    const btnOrderDesc = screen.getByTestId('column-sort-input-desc')
 
+    const btnOrderAsc = screen.getByTestId('column-sort-input-asc')
     userEvent.click(btnOrderAsc)
     userEvent.click(btnOrder)
 
+    const btnOrderDesc = screen.getByTestId('column-sort-input-desc')
     userEvent.click(btnOrderDesc)
     userEvent.click(btnOrder)
 
     userEvent.selectOptions(screen.getByTestId('column-sort'), 'orbital_period')
 
   });
-
-
-
 })
